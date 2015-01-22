493cff5eb2c12cfa4faf7f0fb7800d339577c090
Javier Alfonso <PhoneixSegovia@gmail.com>
2015-01-22 12:23:18 +0100

    Added JFormattedTextField to the WizardContainer tests.

--- test/org/ciscavate/cjwizard/WizardPageTest.java
+++ test/org/ciscavate/cjwizard/WizardPageTest.java
@@ -4,9 +4,11 @@
 package org.ciscavate.cjwizard;
 
 import java.awt.Component;
+import java.text.NumberFormat;
 import java.util.Set;
 
 import javax.swing.JCheckBox;
+import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -23,12 +25,17 @@ import org.junit.Test;
 public class WizardPageTest {
 
    private class CustomTestComponent extends JPanel implements CustomWizardComponent {
+
+      private static final long serialVersionUID = -6865024622146347310L;
+      
+      private String value;
+
       /* (non-Javadoc)
        * @see org.ciscavate.cjwizard.CustomWizardComponent#getValue()
        */
       @Override
       public Object getValue() {
-         return "sample value";
+         return value;
       }
       
       /* (non-Javadoc)
@@ -37,15 +44,18 @@ public class WizardPageTest {
       @Override
       public void setValue(Object o)
       {
-         // no-op
+         this.value = o.toString();
       }
    }
    
    private class TestWizardPage extends WizardPage{
 
+      private static final long serialVersionUID = -4774100981074019102L;
+
       /**
-       * @param title
-       * @param description
+       * Make a basic {@link WizardPage} to test with it.
+       * @param title The title of the page.
+       * @param description The description of the page.
        */
       public TestWizardPage(String title, String description) {
          super(title, description);
@@ -59,12 +69,19 @@ public class WizardPageTest {
          
          CustomTestComponent ctc = new CustomTestComponent();
          ctc.setName("custom comp");
+         ctc.setValue("sample value");
+         
+         JFormattedTextField formated = new JFormattedTextField(
+               NumberFormat.getIntegerInstance());
+         formated.setName("formated field");
+         formated.setValue(Long.valueOf(42l));
          
          add(new JLabel("test label"));
          
          add(field);
          add(box);
          add(ctc);
+         add(formated);
       }
       
    }
@@ -73,7 +90,7 @@ public class WizardPageTest {
     * Verifies that widgets added to a WizardPage with names are located and
     * accessed properly.
     * 
-    * Test method for {@link org.ciscavate.cjwizard.WizardPage#getNamedComponents(org.ciscavate.cjwizard.WizardSettings)}.
+    * Test method for {@link org.ciscavate.cjwizard.WizardPage#getNamedComponents()}.
     */
    @Test
    public void testGetNamedComponents_countComponents() {
@@ -81,7 +98,7 @@ public class WizardPageTest {
       
       Set<Component> components = page.getNamedComponents();
       
-      Assert.assertEquals("wrong number of named components", 3, components.size());
+      Assert.assertEquals("wrong number of named components", 4, components.size());
    }
    
    /**
@@ -103,6 +120,8 @@ public class WizardPageTest {
             settings.keySet().contains("test field"));
       Assert.assertTrue("Key set did not contain needed key.",
             settings.keySet().contains("custom comp"));
+      Assert.assertTrue("Key set did not contain needed key.",
+            settings.keySet().contains("formated field"));
    }
    
    /**
@@ -128,6 +147,9 @@ public class WizardPageTest {
       String ctcValue = (String)settings.get("custom comp");
 
       Assert.assertEquals("Wrong value.", "sample value", ctcValue);
-      
+
+      Long formatVal = (Long)settings.get("formated field");
+      Assert.assertEquals("Wrong value.", Long.valueOf(42l), formatVal);
+
    }
 }
